71de270659a9188aa7858e38e9049c64ad57a257
David Gatti <github@gatti.pl>
2018-06-05 22:52:30 +0000

    First code

---
+++ README.md
@@ -0,0 +1,7 @@
+# Step Function Restart
+
+This repository is used in conjunction with a step function to restart the whole Step Function if one or more limits are reached
+
+# More about us
+
+[Security7](https://www.security7.net/) is a group of security professionals that strive to make security: affordable, scalable and reliable. If you'd like to get in touch with use, go to our [contact](https://www.security7.net/contact/) page.
--- index.js
+++ index.js
@@ -1,4 +1,58 @@
-exports.handler = (event, context, callback) => {
-    // TODO implement
-    callback(null, 'Hello from Lambda');
+let aws = require('aws-sdk');
+let sf = new aws.StepFunctions();
+let moment = require('moment');
+
+//
+//	This Lambda Function is responsabile for restarting the main AWS Lambda
+//
+exports.handler = async (event) => {
+	
+	//
+	//	1.	Get the ARN of the Step Function that needs to be restarted
+	//
+	let step_function_arn = event.step_function_arn;
+	
+	//
+	//	2.	Get the date of today which will become the next date for the 
+	//		Step Function execution
+	//
+	let new_execution_data = moment().format("YYYY-MM-DD");
+	
+	//
+	//	3.	Set default settings when starting the SF we know how much time 
+	//		passed since the invocation, and what is the ARN to restart
+	//
+	let step_function_input_options = {
+		step_function_arn: step_function_arn,
+		execution_date: new_execution_data
+	};
+	
+	//
+	//	4.	Set the option to start a Step Function
+	//
+	let options = {
+		input: step_function_input_options,
+		stateMachineArn: step_function_arn
+	};
+	
+	//
+	//	->	Start the step function
+	//
+	sf.startExecution(options, function(error, data) {
+	  
+		//
+		//	1.	Check if there were SDK errors
+		//
+		if(error)
+		{
+			throw error;
+		}
+		
+		//
+		//	->	Return the default data
+		//
+		return event;
+		
+	});
+	
 };
